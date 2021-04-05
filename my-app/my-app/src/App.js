18a0bb5467d6d05567665ac9053f137d45ff4226
agitirawan <56152326+agitirawan@users.noreply.github.com>
2021-04-05 16:55:15 +0700

    tugas 4

---
+++ my-app/my-app/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Isi from './Isi';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Isi/>
+    </div>
+  );
+}
+
+export default App;
